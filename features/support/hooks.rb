f951c15787f0c64ba47207cab47040f46622d34e
Rafael Vescio <rafael.vescio@sensedata.com.br>
2021-03-09 09:09:30 -0300

    set embed method for ruby version on dockerfile

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,5 +5,8 @@ end
 
 After do |scenario|
   screenshot = page.save_screenshot("logs/screenshots/#{scenario.__id__}.png")
-  attach(screenshot, "png")
+  embed(screenshot, "image/png", "Screenshot")
+  # linha acima WARNING: #embed(file, mime_type, label) is deprecated and will be removed after version 6.0.0. Please use attach(file, media_type) instead.
+  # caso retorne o warning utilize a linha abaixo
+  # attach(screenshot, "png")
 end
